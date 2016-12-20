cfd7950a984c2fad419dfab2a01b1e18837811ec
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2016-12-20 16:40:34 +0100

    lines skippable
    useful to give equilibration times

--- fileOp.hpp
+++ fileOp.hpp
@@ -22,13 +22,16 @@ std::string getNextLine(T &instream)
 std::string getNthWord(const std::string &line, int n);
 
 template<class T>
-Histogram histogramFromStream(T &instream, int num_bins, double lower, double upper, int column=0)
+Histogram histogramFromStream(T &instream, int num_bins, double lower, double upper, int column=0, int skip=0)
 {
     Histogram h(num_bins, lower, upper);
 
+    int ctr = 0;
     while(instream.good())
     {
         std::string line = getNextLine(instream);
+        if(ctr++ < skip)
+            continue;
         if(line.empty())
             continue;
         std::string word = getNthWord(line, column);
@@ -40,11 +43,14 @@ Histogram histogramFromStream(T &instream, int num_bins, double lower, double up
 }
 
 template<class T>
-void bordersFromStream(T &instream, double &lower, double &upper, int column=0)
+void bordersFromStream(T &instream, double &lower, double &upper, int column=0, int skip=0)
 {
+    int ctr = 0;
     while(instream.good())
     {
         std::string line = getNextLine(instream);
+        if(ctr++ < skip)
+            continue;
         if(line.empty())
             continue;
         std::string word = getNthWord(line, column);
