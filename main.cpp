e15c1a2723f037081bbb315438575a8533c93577
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2016-12-21 09:30:59 +0100

    determine borders in parallel

--- main.cpp
+++ main.cpp
@@ -23,28 +23,33 @@ int main(int argc, char** argv)
             o.border_path_vector.push_back(o.data_path_vector.back());
     }
 
-    // TODO: read the files in parallel with openMP
     if(!o.border_path_vector.empty())
     {
         LOG(LOG_INFO) << "determine borders from files (" << o.border_path_vector.size() << " given)";
-        o.lowerBound = 1e300;
-        o.upperBound = -1e300;
-        // unzip, if it ends on .gz
-        for(auto &file : o.border_path_vector)
+
+        double lower = 1e300;
+        double upper = 1e-300;
+
+        #pragma omp parallel for reduction(min:lower), reduction(max:upper)
+        for(size_t i=0; i<o.border_path_vector.size(); ++i)
         {
+            auto &file = o.border_path_vector[i];
             LOG(LOG_INFO) << "read: " << file;
+            // unzip, if it ends on .gz
             if(has_suffix(file, ".gz"))
             {
                 igzstream is(file.c_str());
-                bordersFromStream(is, o.lowerBound, o.upperBound, o.column);
+                bordersFromStream(is, lower, upper, o.column);
             }
             // otherwise try to read it as a normal file
             else
             {
                 std::ifstream is(file.c_str());
-                bordersFromStream(is, o.lowerBound, o.upperBound, o.column);
+                bordersFromStream(is, lower, upper, o.column);
             }
         }
+        o.lowerBound = lower;
+        o.upperBound = upper;
         LOG(LOG_INFO) << "use range [" << o.lowerBound << ", " << o.upperBound<< "]";
     }
 
