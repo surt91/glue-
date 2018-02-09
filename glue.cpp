9049a2be975daf5f7ec38b372d9622e5c71f5705
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2018-02-09 13:10:19 +0100

    fixed missing reference data in glued plot

--- glue.cpp
+++ glue.cpp
@@ -112,9 +112,15 @@ Histogram glueHistograms(const std::vector<Histogram> &hists, const std::vector<
 
         Zs[i] = Zs[i-1] + meanZ;
     }
-    // correct data
+
     // can be plotted in gnuplot with
     // p "glued.dat" u 1:2:-1 w p palette
+    for(size_t j=0; j<corrected_data[0].size(); ++j)
+    {
+        if(std::isfinite(corrected_data[0][j]))
+            osGlued << centers[j] << " " << corrected_data[0][j] << "\n";
+    }
+    osGlued << "\n";
     for(size_t i=1; i<hists.size(); ++i)
     {
         for(size_t j=0; j<corrected_data[i].size(); ++j)
