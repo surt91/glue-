2f364d7923feb4c9e2169f3dcbd2cf4ff4fd0fdc
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2019-07-22 15:40:06 +0200

    function to determine Zs

--- glue.cpp
+++ glue.cpp
@@ -24,6 +24,66 @@ void write_to_stream(std::ofstream &oss, const std::vector<double> &centers, con
     oss << "\n";
 }
 
+
+/** Determine the normalization constants Z
+ *
+ *  These can be used to correct the biased distributions by shifting accodring to
+ *  \f[ \frac{1}{Z_\Theta} P(S) = e^{S/\Theta} P_{\Theta} \f]
+ *
+ * \param hists input histograms, used to determine weights
+ * \param corrected_data right hand side of above equation for every histogram
+ * \param threshold if a bin has fewer entries, ignore it
+ * \param weighted should the means be weighted
+ * \param thetas used temperatures, only for user output
+ */
+std::vector<double> determineZ(const std::vector<Histogram> &hists, const std::vector<std::vector<double>> &corrected_data, int threshold, bool weighted, const std::vector<double> &thetas)
+{
+    std::vector<double> Zs(hists.size(), 0);
+    for(size_t i=1; i<hists.size(); ++i)
+    {
+        const auto &count1 = hists[i-1].get_data();
+        const auto &count2 = hists[i].get_data();
+        const auto &data1 = corrected_data[i-1];
+        const auto &data2 = corrected_data[i];
+
+        std::vector<double> Z;
+        std::vector<double> weight; // how to weight the data, to get the mean of Z
+        // get region of overlap
+        // assumes temperatures are ordered
+        for(size_t j=0; j<data1.size(); ++j)
+        {
+            if(count1[j] > threshold && count2[j] > threshold)
+            {
+                Z.push_back(data1[j]-data2[j]);
+                // weight the Z: more weight, if both datasets have many entries
+                if(weighted)
+                    weight.push_back(std::min(hists[i].get_data()[j], hists[i].get_data()[j]));
+                else // equal weight, if data originates from WL
+                    weight.push_back(1);
+            }
+        }
+        double meanZ;
+        if(Z.size())
+        {
+            meanZ = weighted_mean(Z, weight);
+        }
+        else if(weighted)
+        {
+            meanZ = 0;
+            LOG(LOG_WARNING) << "no overlap between T = " << thetas[i-1] << " and T = " << thetas[i];
+        }
+        else
+        {
+            meanZ = 0;
+            LOG(LOG_WARNING) << "no overlap between [" << (i-1)
+                <<  "] and [" << i <<  "]";
+        }
+
+        Zs[i] = Zs[i-1] + meanZ;
+    }
+    return Zs;
+}
+
 /** Determine the normalization constants \f$ Z_\Theta \f$.
  *
  *  The histograms need to have the same borders.
@@ -45,12 +105,15 @@ Histogram glueHistograms(const std::vector<Histogram> &hists, const std::vector<
     for(auto &h : hists)
         osHist << h.ascii_table() << "\n";
 
+    // perform weighting only for temperature based sheme
+    const bool weighted = !thetas.empty();
+
     // centers of all histograms should be equal
     const auto &centers = hists[0].centers();
 
     std::vector<std::vector<double>> corrected_data;
     // if no temperatures are given, do just merge the histograms
-    if(!thetas.empty())
+    if(weighted)
     {
         for(size_t i=0; i<hists.size(); ++i)
         {
@@ -89,53 +152,7 @@ Histogram glueHistograms(const std::vector<Histogram> &hists, const std::vector<
         }
     }
 
-    std::vector<double> Zs(hists.size(), 0);
-    for(size_t i=1; i<hists.size(); ++i)
-    {
-        const auto &count1 = hists[i-1].get_data();
-        const auto &count2 = hists[i].get_data();
-        const auto &data1 = corrected_data[i-1];
-        const auto &data2 = corrected_data[i];
-
-        std::vector<double> Z;
-        std::vector<double> weight; // how to weight the data, to get the mean of Z
-        // get region of overlap
-        // assumes temperatures are ordered
-        for(size_t j=0; j<data1.size(); ++j)
-        {
-            if(count1[j] > threshold && count2[j] > threshold)
-            {
-                Z.push_back(data1[j]-data2[j]);
-                // weight the Z: more weight, if both datasets have many entries
-                if(!thetas.empty())
-                {
-                    weight.push_back(std::min(hists[i].get_data()[j], hists[i].get_data()[j]));
-                }
-                else // equal weight, if data originates from WL
-                {
-                    weight.push_back(1);
-                }
-            }
-        }
-        double meanZ;
-        if(Z.size())
-        {
-            meanZ = weighted_mean(Z, weight);
-        }
-        else if(!thetas.empty())
-        {
-            meanZ = 0;
-            LOG(LOG_WARNING) << "no overlap between T = " << thetas[i-1] << " and T = " << thetas[i];
-        }
-        else
-        {
-            meanZ = 0;
-            LOG(LOG_WARNING) << "no overlap between [" << (i-1)
-                <<  "] and [" << i <<  "]";
-        }
-
-        Zs[i] = Zs[i-1] + meanZ;
-    }
+    std::vector<double> Zs = determineZ(hists, corrected_data, threshold, weighted, thetas);
 
     for(size_t i=1; i<hists.size(); ++i)
         for(size_t j=0; j<corrected_data[i].size(); ++j)
@@ -152,7 +169,7 @@ Histogram glueHistograms(const std::vector<Histogram> &hists, const std::vector<
         {
             double total = 0;
             double total_weight = 0;
-            if(!thetas.empty())
+            if(weighted)
             {
                 for(size_t i=0; i<hists.size(); ++i)
                 {
